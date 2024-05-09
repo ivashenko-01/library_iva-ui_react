03eb8a4725994192e16258f6503e63018b76f8d2
Евгений Иващенко <ivashenko-01@mail.ru>
2024-05-09 22:00:34 +0300

    Компонент Input

--- src/shared/ui/Input/ui/Input.jsx
+++ src/shared/ui/Input/ui/Input.jsx
@@ -1,4 +1,4 @@
-import styled from './input.module.css';
+import styled from './input.module.scss';
 
 export function Input({
     children,
@@ -14,39 +14,61 @@ export function Input({
     addBefore,
     addAfter,
     addAfterButton,
-    isError,
+    isErrorInput,
+    isErrorMessage,
     isValid,
+    onClickAfterButton,
+    afterColor,
 }) {
+    if (!onClickAfterButton) {
+        onClickAfterButton = () => {};
+    }
     return (
         <>
-            <div className={styled['the-input-field']}>
-                <div className={styled['the-input-field__wrapper']}>
-                    <div>
-                        {isBefore && <div className={styled['the-input-field__before']}>{addBefore}</div>}
-                        <input
-                            type="text"
-                            className={`${styled[`the-input-field__input`]} ${styled[align]}`}
-                            placeholder=" "
-                            name={forName}
-                            value={value}
-                            disabled={disabled}
-                            onChange={(e) => onChangeValue(e.target.value)}
-                        />
+            <div className={styled['the-input']}>
+                <div className={styled['the-input-field']}>
+                    <div className={styled['the-input-field__wrapper']}>
+                        <div>
+                            {isBefore && <div className={styled['the-input-field__before']}>{addBefore}</div>}
+                            <input
+                                type="text"
+                                className={`${styled[`the-input-field__input`]} ${styled[isErrorInput ? 'error-true' : 'error-false']} ${styled[align]} ${isBefore && styled[`the-input-field__input-add-before`]} ${isAfter && styled[`the-input-field__input-add-after`]}`}
+                                placeholder=" "
+                                name={forName}
+                                value={value}
+                                disabled={disabled}
+                                onChange={(e) => onChangeValue(e.target.value)}
+                            />
 
-                        {enabledLabel && (
-                            <label className={styled[`the-input-field__label`]} for={forName}>
-                                {children}
-                            </label>
-                        )}
+                            {enabledLabel && (
+                                <label
+                                    className={`${styled[`the-input-field__label`]} ${isBefore && styled[`the-input-field__label-add-before`]}`}
+                                    htmlFor={forName}
+                                >
+                                    {children}
+                                </label>
+                            )}
 
-                        {isAfter && <div className={styled['the-input-field__after']}>{addAfter}</div>}
-                        {isAfterButton && (
-                            <div className={styled['the-input-field__after-button']}>
-                                <button>{addAfterButton}</button>
-                            </div>
-                        )}
+                            {isAfter && (
+                                <div
+                                    className={`${styled['the-input-field__after']} ${styled[`the-input-field__after--${afterColor}`]}`}
+                                >
+                                    {addAfter}
+                                </div>
+                            )}
+                            {isAfterButton && (
+                                <div className={styled['the-input-field__after-button']}>
+                                    {value && <button onClick={() => onClickAfterButton()}>{addAfterButton}</button>}
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
+
+                {isErrorInput &&
+                    isErrorMessage.map((error) => (
+                        <div className={styled[`the-input-field__error-message`]}>{error}</div>
+                    ))}
             </div>
         </>
     );
